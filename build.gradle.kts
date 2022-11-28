dd12f43f9f9c21ceefa2a1315fef681513a9524d
ALEXEY ZVEREV <ozonophore@gmail.com>
2022-11-28 19:41:15 +0300

    OCTOPUS-11 Add build workflow (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,29 @@ plugins {
     `maven-publish`
     idea
     id("org.jetbrains.kotlin.jvm") apply (false)
+    id("io.github.gradle-nexus.publish-plugin")
+    `signing`
 }
 
 repositories {
     mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
 }
 
 subprojects {
@@ -36,6 +55,27 @@ subprojects {
         publications {
             create<MavenPublication>("maven") {
                 from(components["java"])
+                pom {
+                    name.set(project.name)
+                    description.set("Octopus module(${project.name}) for cloud-commons")
+                    url.set("https://github.com/octopusden/octopus-cloud-commons.git")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/octopusden/octopus-rm-gradle-plugin.git")
+                        connection.set("scm:git://github.com/octopusden/octopus-cloud-commons.git")
+                    }
+                    developers {
+                        developer {
+                            id.set("octopus")
+                            name.set("octopus")
+                        }
+                    }
+                }
             }
         }
     }
