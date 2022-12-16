fa7a1749af6b9dc5bd17af708fa2bcad7e6886bb
Alexey Zverev <ozonophore@gmail.com>
2022-12-16 10:37:37 +0200

    Fix empty publishing

--- security-common/build.gradle.kts
+++ security-common/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    implementation(enforcedPlatform("org.springframework.cloud:spring-cloud-dependencies:${project.properties["spring-cloud.version"]}"))
-    implementation(enforcedPlatform("org.springframework.boot:spring-boot-dependencies:${project.properties["spring-boot.version"]}"))
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${project.properties["spring-cloud.version"]}"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:${project.properties["spring-boot.version"]}"))
     implementation("org.springframework.security:spring-security-config")
     implementation("org.springframework.security:spring-security-oauth2-resource-server")
     implementation("org.springframework.security:spring-security-oauth2-jose")
