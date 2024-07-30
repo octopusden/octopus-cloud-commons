7fea93c7fbb46f2f3aab86209d98fb850b170454
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-07-30 17:05:38 +0300

    Update versions for spring, gradle, kotlin

--- security-common/src/main/kotlin/org/octopusden/cloud/commons/security/config/CloudCommonWebSecurityConfig.kt
+++ security-common/src/main/kotlin/org/octopusden/cloud/commons/security/config/CloudCommonWebSecurityConfig.kt
@@ -1,5 +1,6 @@
 package org.octopusden.cloud.commons.security.config
 
+import javax.annotation.PostConstruct
 import org.octopusden.cloud.commons.security.SecurityService
 import org.octopusden.cloud.commons.security.client.AuthServerClient
 import org.octopusden.cloud.commons.security.converter.UserInfoGrantedAuthoritiesConverter
@@ -9,31 +10,40 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
-import javax.annotation.PostConstruct
+import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @EnableConfigurationProperties(SecurityProperties::class)
-abstract class CloudCommonWebSecurityConfig(private val authServerClient: AuthServerClient) : WebSecurityConfigurerAdapter() {
+abstract class CloudCommonWebSecurityConfig(private val authServerClient: AuthServerClient) {
     @Autowired
     protected lateinit var securityProperties: SecurityProperties
 
     @PostConstruct
     fun printLoadedRoles() {
-        log.info("Loaded roles: ${securityProperties.roles.map { (role, permissions) -> "$role: [${permissions.joinToString() }]" }.joinToString("\n")}")
+        log.info(
+            "Loaded roles: {}",
+            securityProperties.roles
+                .map { (role, permissions) -> "$role: [${permissions.joinToString()}]" }
+                .joinToString("\n")
+        )
     }
 
     @Bean
-    open fun securityService(): SecurityService = SecurityService(securityProperties)
-
-    override fun configure(http: HttpSecurity) {
+    open fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .authorizeRequests()
-            .antMatchers("/actuator/**").permitAll()
+            .antMatchers(
+                "/",
+                "/actuator/**",
+                "/v2/api-docs",
+                "/v3/api-docs",
+                "/v3/api-docs/swagger-config",
+                "/swagger-resources/**",
+                "/swagger-ui/**"
+            ).permitAll()
             .anyRequest().authenticated()
             .and().oauth2ResourceServer {
                 it.jwt()
@@ -48,18 +58,11 @@ abstract class CloudCommonWebSecurityConfig(private val authServerClient: AuthSe
                         })
             }
             .cors().disable()
+        return http.build()
     }
 
-    override fun configure(web: WebSecurity) {
-        web.ignoring()
-            .antMatchers("/",
-                "/actuator/**",
-                "/v2/api-docs",
-                "/v3/api-docs",
-                "/v3/api-docs/swagger-config",
-                "/swagger-resources/**",
-                "/swagger-ui/**")
-    }
+    @Bean
+    open fun securityService(): SecurityService = SecurityService(securityProperties)
 
     companion object {
         private val log = LoggerFactory.getLogger(CloudCommonWebSecurityConfig::class.java)
