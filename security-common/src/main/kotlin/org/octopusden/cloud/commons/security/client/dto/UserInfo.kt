68d928c1b0852a38c3b72f651e55c07414a4552f
Mark Rode <54143576+rodemark@users.noreply.github.com>
2025-10-28 10:34:37 +0300

    Change UserInfo data model (#43)

--- security-common/src/main/kotlin/org/octopusden/cloud/commons/security/client/dto/UserInfo.kt
+++ security-common/src/main/kotlin/org/octopusden/cloud/commons/security/client/dto/UserInfo.kt
@@ -1,6 +1,11 @@
 package org.octopusden.cloud.commons.security.client.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
+
 data class UserInfo(
+    @field:JsonFormat(with = [JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY])
     val roles: Collection<String>,
+
+    @field:JsonFormat(with = [JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY])
     val groups: Collection<String>
 )
