b04e944b051c5738935a95af000735343ae16e9d
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2025-12-25 16:34:49 +0300

    Add JsonProperty annotations for fields in UserInfo and make signing optional.

--- security-common/src/main/kotlin/org/octopusden/cloud/commons/security/client/dto/UserInfo.kt
+++ security-common/src/main/kotlin/org/octopusden/cloud/commons/security/client/dto/UserInfo.kt
@@ -1,11 +1,14 @@
 package org.octopusden.cloud.commons.security.client.dto
 
 import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UserInfo(
+    @JsonProperty("roles")
     @field:JsonFormat(with = [JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY])
     val roles: Collection<String>,
 
+    @JsonProperty("groups")
     @field:JsonFormat(with = [JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY])
     val groups: Collection<String>
 )
